37f1f01ac46da71531866eb043a2faa08dcb2cc4
tusharsharma1996 <tusharsharma369@yahoo.in>
2017-09-04 17:06:01 +0530

    My app,first commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+
+
+
+
+#password tushar2042
+# Register your models here.
